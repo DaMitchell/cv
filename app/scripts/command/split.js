2d53430a54e4ac32ed7c90a179b5b5593d9a70b3
Dan Mitchell <danmitchell@ntlworld.com>
2015-05-22 14:02:36 +0100

    can not split and exit a console correctly

--- app/scripts/command/split.js
+++ app/scripts/command/split.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//import Events from 'events';
+import Events from 'events';
 
 export default function() {
     var api;
@@ -17,18 +17,49 @@ export default function() {
         return 'Split the console window';
     }
 
-    function execute() {
-        var left = $('<div>').addClass('left');
-        var right = $('<div>').addClass('right').append(api.config.containerClone.clone());
+    function execute(args) {
+        var direction;
 
-        $(api.config.container).parent().append(left);
-        $(api.config.container).parent().append(right);
+        switch(args[0]) {
+            case 'horiz':
+            case 'vert':
+                direction = args[0];
+                break;
+        }
 
-        $(api.config.container).appendTo(left);
+        if(direction) {
+            if(direction === 'vert') {
+                var left = $('<div>').addClass('left');
+                var right = $('<div>').addClass('right').append(api.config.containerClone.clone());
 
-        api.hierarchy.addChild(new require('console')['default'](right.find('.console'), {
-            parent: api
-        }));
+                $(api.config.container).parent().append(left);
+                $(api.config.container).parent().append(right);
+
+                $(api.config.container).appendTo(left);
+
+                api.hierarchy.addChild(new require('console')['default'](right.find('.console'), {
+                    parent: api
+                }));
+            } else if(direction === 'horiz') {
+                var top = $('<div>').addClass('top');
+                var bottom= $('<div>').addClass('bottom').append(api.config.containerClone.clone());
+
+                $(api.config.container).parent().append(top);
+                $(api.config.container).parent().append(bottom);
+
+                $(api.config.container).appendTo(top);
+
+                api.hierarchy.addChild(new require('console')['default'](bottom.find('.console'), {
+                    parent: api
+                }));
+            }
+
+            return true;
+        }
+
+        $(api).trigger(Events.OUTPUT, {
+            content: 'Sorry "' + direction + '" is an invalid split direction.'
+        });
     }
 
     return {
